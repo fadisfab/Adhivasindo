015bc7e43ba4d9b8c31facf70c873f2eca1409f1
fadisfab <novalfadillah3@gmail.com>
2025-12-14 16:24:41 +0700

    Add files via upload

---
+++ career_backend_mochamad fadillah wijaya.sql
@@ -0,0 +1,254 @@
+/*
+ Navicat Premium Dump SQL
+
+ Source Server         : localhost_5432
+ Source Server Type    : PostgreSQL
+ Source Server Version : 170005 (170005)
+ Source Host           : localhost:5432
+ Source Catalog        : adhivasindo
+ Source Schema         : public
+
+ Target Server Type    : PostgreSQL
+ Target Server Version : 170005 (170005)
+ File Encoding         : 65001
+
+ Date: 14/12/2025 16:24:10
+*/
+
+
+-- ----------------------------
+-- Sequence structure for failed_jobs_id_seq
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "public"."failed_jobs_id_seq";
+CREATE SEQUENCE "public"."failed_jobs_id_seq" 
+INCREMENT 1
+MINVALUE  1
+MAXVALUE 9223372036854775807
+START 1
+CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for migrations_id_seq
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "public"."migrations_id_seq";
+CREATE SEQUENCE "public"."migrations_id_seq" 
+INCREMENT 1
+MINVALUE  1
+MAXVALUE 2147483647
+START 1
+CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for personal_access_tokens_id_seq
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "public"."personal_access_tokens_id_seq";
+CREATE SEQUENCE "public"."personal_access_tokens_id_seq" 
+INCREMENT 1
+MINVALUE  1
+MAXVALUE 9223372036854775807
+START 1
+CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for pool_user_id_seq
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "public"."pool_user_id_seq";
+CREATE SEQUENCE "public"."pool_user_id_seq" 
+INCREMENT 1
+MINVALUE  1
+MAXVALUE 9223372036854775807
+START 1
+CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for users_id_seq
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "public"."users_id_seq";
+CREATE SEQUENCE "public"."users_id_seq" 
+INCREMENT 1
+MINVALUE  1
+MAXVALUE 9223372036854775807
+START 1
+CACHE 1;
+
+-- ----------------------------
+-- Table structure for failed_jobs
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."failed_jobs";
+CREATE TABLE "public"."failed_jobs" (
+  "id" int8 NOT NULL DEFAULT nextval('failed_jobs_id_seq'::regclass),
+  "uuid" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "connection" text COLLATE "pg_catalog"."default" NOT NULL,
+  "queue" text COLLATE "pg_catalog"."default" NOT NULL,
+  "payload" text COLLATE "pg_catalog"."default" NOT NULL,
+  "exception" text COLLATE "pg_catalog"."default" NOT NULL,
+  "failed_at" timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP
+)
+;
+
+-- ----------------------------
+-- Table structure for migrations
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."migrations";
+CREATE TABLE "public"."migrations" (
+  "id" int4 NOT NULL DEFAULT nextval('migrations_id_seq'::regclass),
+  "migration" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "batch" int4 NOT NULL
+)
+;
+
+-- ----------------------------
+-- Table structure for password_resets
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."password_resets";
+CREATE TABLE "public"."password_resets" (
+  "email" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "token" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "created_at" timestamp(0)
+)
+;
+
+-- ----------------------------
+-- Table structure for personal_access_tokens
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."personal_access_tokens";
+CREATE TABLE "public"."personal_access_tokens" (
+  "id" int8 NOT NULL DEFAULT nextval('personal_access_tokens_id_seq'::regclass),
+  "tokenable_type" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "tokenable_id" int8 NOT NULL,
+  "name" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "token" varchar(64) COLLATE "pg_catalog"."default" NOT NULL,
+  "abilities" text COLLATE "pg_catalog"."default",
+  "last_used_at" timestamp(0),
+  "created_at" timestamp(0),
+  "updated_at" timestamp(0)
+)
+;
+
+-- ----------------------------
+-- Table structure for pool_users
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."pool_users";
+CREATE TABLE "public"."pool_users" (
+  "id" int8 NOT NULL DEFAULT nextval('pool_user_id_seq'::regclass),
+  "nama" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "nim" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "ymd" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "data" json,
+  "created_at" timestamp(0),
+  "updated_at" timestamp(0)
+)
+;
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."users";
+CREATE TABLE "public"."users" (
+  "id" int8 NOT NULL DEFAULT nextval('users_id_seq'::regclass),
+  "name" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "email" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "email_verified_at" timestamp(0),
+  "password" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "remember_token" varchar(100) COLLATE "pg_catalog"."default",
+  "created_at" timestamp(0),
+  "updated_at" timestamp(0),
+  "token" varchar(80) COLLATE "pg_catalog"."default"
+)
+;
+
+-- ----------------------------
+-- Alter sequences owned by
+-- ----------------------------
+ALTER SEQUENCE "public"."failed_jobs_id_seq"
+OWNED BY "public"."failed_jobs"."id";
+SELECT setval('"public"."failed_jobs_id_seq"', 1, false);
+
+-- ----------------------------
+-- Alter sequences owned by
+-- ----------------------------
+ALTER SEQUENCE "public"."migrations_id_seq"
+OWNED BY "public"."migrations"."id";
+SELECT setval('"public"."migrations_id_seq"', 6, true);
+
+-- ----------------------------
+-- Alter sequences owned by
+-- ----------------------------
+ALTER SEQUENCE "public"."personal_access_tokens_id_seq"
+OWNED BY "public"."personal_access_tokens"."id";
+SELECT setval('"public"."personal_access_tokens_id_seq"', 1, false);
+
+-- ----------------------------
+-- Alter sequences owned by
+-- ----------------------------
+ALTER SEQUENCE "public"."pool_user_id_seq"
+OWNED BY "public"."pool_users"."id";
+SELECT setval('"public"."pool_user_id_seq"', 421, true);
+
+-- ----------------------------
+-- Alter sequences owned by
+-- ----------------------------
+ALTER SEQUENCE "public"."users_id_seq"
+OWNED BY "public"."users"."id";
+SELECT setval('"public"."users_id_seq"', 3, true);
+
+-- ----------------------------
+-- Uniques structure for table failed_jobs
+-- ----------------------------
+ALTER TABLE "public"."failed_jobs" ADD CONSTRAINT "failed_jobs_uuid_unique" UNIQUE ("uuid");
+
+-- ----------------------------
+-- Primary Key structure for table failed_jobs
+-- ----------------------------
+ALTER TABLE "public"."failed_jobs" ADD CONSTRAINT "failed_jobs_pkey" PRIMARY KEY ("id");
+
+-- ----------------------------
+-- Primary Key structure for table migrations
+-- ----------------------------
+ALTER TABLE "public"."migrations" ADD CONSTRAINT "migrations_pkey" PRIMARY KEY ("id");
+
+-- ----------------------------
+-- Indexes structure for table password_resets
+-- ----------------------------
+CREATE INDEX "password_resets_email_index" ON "public"."password_resets" USING btree (
+  "email" COLLATE "pg_catalog"."default" "pg_catalog"."text_ops" ASC NULLS LAST
+);
+
+-- ----------------------------
+-- Indexes structure for table personal_access_tokens
+-- ----------------------------
+CREATE INDEX "personal_access_tokens_tokenable_type_tokenable_id_index" ON "public"."personal_access_tokens" USING btree (
+  "tokenable_type" COLLATE "pg_catalog"."default" "pg_catalog"."text_ops" ASC NULLS LAST,
+  "tokenable_id" "pg_catalog"."int8_ops" ASC NULLS LAST
+);
+
+-- ----------------------------
+-- Uniques structure for table personal_access_tokens
+-- ----------------------------
+ALTER TABLE "public"."personal_access_tokens" ADD CONSTRAINT "personal_access_tokens_token_unique" UNIQUE ("token");
+
+-- ----------------------------
+-- Primary Key structure for table personal_access_tokens
+-- ----------------------------
+ALTER TABLE "public"."personal_access_tokens" ADD CONSTRAINT "personal_access_tokens_pkey" PRIMARY KEY ("id");
+
+-- ----------------------------
+-- Uniques structure for table pool_users
+-- ----------------------------
+ALTER TABLE "public"."pool_users" ADD CONSTRAINT "pool_user_nim_unique" UNIQUE ("nim");
+
+-- ----------------------------
+-- Primary Key structure for table pool_users
+-- ----------------------------
+ALTER TABLE "public"."pool_users" ADD CONSTRAINT "pool_user_pkey" PRIMARY KEY ("id");
+
+-- ----------------------------
+-- Uniques structure for table users
+-- ----------------------------
+ALTER TABLE "public"."users" ADD CONSTRAINT "users_email_unique" UNIQUE ("email");
+ALTER TABLE "public"."users" ADD CONSTRAINT "users_token_unique" UNIQUE ("token");
+
+-- ----------------------------
+-- Primary Key structure for table users
+-- ----------------------------
+ALTER TABLE "public"."users" ADD CONSTRAINT "users_pkey" PRIMARY KEY ("id");
